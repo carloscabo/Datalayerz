18d3cadecbae8417e6c5a8cf2f86d5d9769998cd
Carlos Cabo <carloscabo@gmail.com>
2017-08-09 21:14:44 +0200

    Initial release and readme

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
 # Datalayerz
-Google Tag Manager and datalayer events and variables manager
+Google Tag Manager (GTM) and datalayer events and variables manager.
+
+# Requirements
+
+- jQuery 1.7.1+
+
+You must have your GTM correctly integrated with yout proyect. Please read carefully hot yo properly add GTM to your proyect.
+
+# Usage steps
+
+Include the JS library ( `datalayerz.js` ) in your project.
+
+Add a `data-datalayer` attribute on those elements that you want to be binded to common events ( _click, change, ..._ ).
+
+The `data-datalayer` must contain a **valid JSON string** that will define all the datalayer variables that will be sent and the **triggering event** also:
+
+````
+data-datalayer='[{"trigger":"click","eventCategory":"Menu","eventAction":"Search"}]'
+````
+
+As you can see in the sample the JSON object is an array. This allows you to attach several events to the same html element, that may be useful some times.
+
+````html
+<!-- Passing 2 events (click, change) to teh same element -->
+<select name="lang" id="main-lang" data-datalayer='[{"trigger":"change", "eventCategory":"Category1", "eventAction":"Language changed"}, {"trigger":"click", "eventCategory":"Category2", "eventAction":"Open lang selector"}]'>
+  <option value="ES">ES</option>
+  <option value="EN">EN</option>
+````
+
+# Samples
+
+## 1. Binding a simple click event
+
+The easiest GTM event binding:
+
+````html
+<div class="actions">
+  <ul>
+    <li class="search" data-datalayer='[{"trigger":"click","eventCategory":"Menu","eventAction":"Show search"}]' >
+      <img src="search.png" alt="Search icon">
+    </li>
+    <!-- ... -->
+````
+
+## 2. Binding two events
+
+````html
+<!-- Passing 2 events (click, change) to teh same element -->
+<select name="lang" id="main-lang" data-datalayer='[{"trigger":"change", "eventCategory":"Category1", "eventAction":"Language changed"}, {"trigger":"click", "eventCategory":"Category2", "eventAction":"Open lang selector"}]'>
+  <option value="ES">ES</option>
+  <option value="EN">EN</option>
+````
+
+## 3. Sending information from the html tag itself or other elements in page
+
+You can send some information from an element passing an string containing a _jQuery selector_ that will be evaluated when the event is triggered.
+
+You can refer to the element itself using the `$(el)` selector.
+
+Imagine that tou want to send a GTM variaable when user changes the language of the application. You can add a `data-datalayer` as follows:
+
+````html
+<!-- Passing 2 events (click, change) to teh same element -->
+<select name="lang" id="main-lang" data-datalayer='[{"trigger":"change", "eventCategory":"Category1", "eventAction":"Language changed", "eventLabel":"$(el).val()"}]'>
+  <option value="ES">ES</option>
+  <option value="EN">EN</option>
+````
+
+Sending the text of a link its very easy too:
+
+````html
+<a href="#" id="btn" data-datalayer='[{"trigger":"click", "eventCategory":"Category", "eventAction":"Action", "eventLabel":"$(el).text()"}]'>Text of the link</a>
+````
+
+And you can even send the content of any other attribute present in the element this way ( even another data-attr ):
+
+````html
+<a href="#" id="btn" data-datalayer='[{"trigger":"click", "eventCategory":"Category", "eventAction":"Action", "eventLabel":"$(el).attr(\"id\")"}]'>Text of the link</a>
+````
+
+| :warning: **Notice** |
+| :--- |
+| For security reasons you can execute a reduced subset of jQuery functions only!
+- .text()
+- .val()
+- .attr( "attribute" )
+- .data( "identifier" )
+
+|
---
+++ src/js/datalayerz.js
@@ -0,0 +1,136 @@
+/*!
+  Datalayerz v.0.5
+  GTM vars and events manager
+  Authors:
+    Carlos Cabo
+    Victor Ortíz
+    Julia Vallina
+
+  Help and samples: https://github.com/carloscabo/Datalayerz
+*/
+(function ($, undefined) {
+  'use strict';
+
+  // Prevent several initializations
+  if (typeof window.Datalayerz !== 'undefined') {
+    return;
+  }
+
+  var
+    $doc = $(document);
+
+  function init($content) {
+    if (typeof $content === 'undefined') {
+      $content = $('body')
+    };
+
+    $content.find('[data-datalayer-children]').each( function( idx, el ) {
+      var
+        $el = $(el),
+        data = $el.attr('data-datalayer-children');
+      $el.children().each( function( idx, child ) {
+        $(child).attr('data-datalayer', data);
+      });
+      $el.removeAttr('data-datalayer-children'); //.removeData('datalayerChildren');
+    });
+
+    $content.find('[data-datalayer]').each( function( idx, el ) {
+      var
+        dat = $(el).data('datalayer');
+      // First evet has a trigger
+      if ( dat.length && typeof dat[0].trigger !== 'undefined') {
+        activate_events( el );
+      }
+    });
+
+  }
+
+  function activate_events( el ) {
+
+    var
+      // Element
+      $el = $(el),
+      // Original data unmodified
+      data_events = $el.data('datalayer');
+
+    $.each( data_events, function( idx, data_event ){
+
+      $el.on( data_event.trigger, function (e) {
+        e.preventDefault(); // DEBUG
+
+        // Copy to be send
+        var data_event_copy = {};
+        data_event_copy = $.extend({}, data_event );
+
+        // General
+        $.each( data_event_copy, function( key, val ){
+
+          val = val.replace(/\"/g, "\""); // Unescape
+          // console.log(key); console.log(val);
+          // debugger;
+
+          var
+            // Is there a value like {{page.hierarchy}}
+            m = val.match(/^{{(.*)}}$/),
+
+            // jQuery method on element $el.text()
+            f = val.match(/^\$\((?:")?([^"]+)(?:")?\)\.(text|val|data|attr)\((?:")?([^"]+)?(?:")?\)$/);
+
+          // Check for existance of dataLayer variable
+          // {{page.hierarchy}}
+          if ( m && m[1] && dataLayer && dataLayer[0] && dataLayer[0][ m[1] ] ) {
+            data_event_copy[key] = dataLayer[0][ m[1] ];
+          }
+
+          // Is a JQuery function?
+          // Must be in a subset of allowed JS fuctions
+          // $("#element-id_or-id").attr("data-attr")
+
+          // Matches
+          // 1.	#element-id_or-id // el is an especial case
+          // 2. attr              // jQuery method
+          // 3. data-attr         // param to the jQuery method
+
+          else if (f && f[1] && f[2]) { // && typeof $.fn[ f[2] ] === 'function'
+            // data_event_copy[key] = $el[ f[1] ]();
+            var
+              // $(el) is magical
+              $sel = ( f[1] === 'el' ) ? $(el) : $( f[1] ),
+              str = '';
+
+            if ( $sel.length ) {
+              if (typeof f[3] !== 'undefined' ) {
+                str = $sel[f[2]]( f[3] );
+              } else {
+                // $.text() dont work fine passing an undefined param... :-/
+                str = $sel[ f[2] ]();
+              }
+            }
+            data_event_copy[key] = str;
+
+          }
+
+          // is a global function?
+          else if ( typeof window[key] === 'function') {
+            data_event_copy[key] = window[key]();
+          }
+
+        });
+        dataLayer.push( data_event_copy );
+      });
+
+    });
+
+  }
+
+  // Public methods
+  window.Datalayerz = {
+    init: init
+  };
+
+}(jQuery));
+
+// Autoinit in domready
+$(function(){
+  window.Datalayerz.init();
+});
